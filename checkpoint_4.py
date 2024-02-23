9ac4a13af0a8e6977d0cb79934772a6e4210b93d
imartinez620 <info.imartinez@gmail.com>
2024-02-23 10:09:08 +0100

    exercise_1

---
+++ checkpoint_4.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+
+#Exercise 1: Create a list, tuple, float, integer, decimal, and dictionary.
+
+shooping_list = ['milk', 'eggs', 'sugar', 'flour', 'baking_powder']
+recipie_tuple = ('Sponge Cake Recipe', 'Ingredients', 'Process')
+milk_float = 0.75 
+sugar_integer = 300 
+baking_powder_decimal = Decimal(1/7) 
+process_dictionary = {
+	'temperature': 180,
+	'time': 40,
+	'air_flow': 'none',
+	'function': ('top', 'bottom', 'both')
+}
+
+
+
+
